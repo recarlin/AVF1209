d99ef10fca678454db205d6ddc0986bf313e7ab6
Russell Carlin <recarlin@fullsail.com>
2012-08-05 00:21:04 -0400

    Try this again.

---
+++ EverydayEpics_Android/src/com/fullsail/everydayepics/EverydayEpics.java
@@ -0,0 +1,13 @@
+package com.fullsail.everydayepics;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class EverydayEpics extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/launcher.html");
+    }
+}
