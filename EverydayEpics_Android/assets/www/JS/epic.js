48a3fdf52f33b4786c33590a29b655a6bbb543cb
Russell Carlin <recarlin@fullsail.com>
2012-08-07 22:39:57 -0400

    Got all the pages laid out, just need some JS and a form to go with it. Then features.

---
+++ EverydayEpics_Android/assets/www/JS/epic.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$('#buttonNew').click(function(){
+     	window.location = "newStash.html";
+    	return false;
+    });
+	$('buttonStash').click(function(){
+		window.location="page2.html";
+		return false;
+	});
+	
+});
